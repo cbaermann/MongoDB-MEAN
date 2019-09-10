3ef01826ea2d541aea1b9c6c64d41bf77dd194d2
Cody Baermann <cbaermann@comcast.net>
2019-09-10 13:44:11 -0700

    initial commit

--- 1955API/server/models/people.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/FoxDashboard', {useNewUrlParser: true});
-
-const PeopleSchema = new mongoose.Schema({
-    name: {type: String, required: [true, "you must enter a name"], minlength: 2},
-    age: {type: Number, required: true, minlength: 1},
-}, {timestamps: true})
-
-moduel.exports = mongoose.model("people", PeopleSchema)
